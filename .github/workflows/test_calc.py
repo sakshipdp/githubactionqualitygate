5a9eb88a6e6a613bba7c747a2b0c1a2310f5acef
sakshipdp <sakshi.63001@gmail.com>
2025-10-28 22:01:57 +0530

    testing3

--- .github/workflows/test_calc.py
+++ .github/workflows/test_calc.py
@@ -1,16 +1,21 @@
 import pytest
-
-from calc import add,sub,mul,div
+# tests/test_calc.py
+from calc import add
 
 def test_add():
+    assert add(2, 3) == 5
+
+#from calc import add,sub,mul,div
+
+#def test_add():
 #compare actual output and expected output
-   assert add(2,3) == 5
+  # assert add(2, 3) == 5
 
 def test_sub():
-   assert sub(3,1) == 2
+   assert sub(3, 1) == 2
 
 def test_mul():
-   assert mul(4,2) == 8
+   assert mul(4, 2) == 8
 
 def test_div():
-   assert div(4,2) == 2
+   assert div(4, 2) == 2
